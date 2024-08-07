f5ed8c572eb95cb34c6bd2dff90849d86abd0963
willclem100 <will.clem@biteninja.com>
2024-08-06 22:37:42 -0700

    Begin lesson 18

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import Link from 'next/link'
+import Link from "next/link";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 
 const PropertiesPage = () => {
-  return <div>
-    <h1 className="text-3xl">Properties</h1>
-    <Link href="/">Go Home</Link>
-  </div>;
+  return (
+    <section className="px-4 py-6">
+      <div className="px-4 py-6 m-auto container-xl lg:container">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default PropertiesPage;
