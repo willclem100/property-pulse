d32ba416ae5d60281ff8ae7d34bbbed5b8975e45
willclem100 <will.clem@biteninja.com>
2024-08-20 19:06:40 -0500

    Start Lesson 26

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,8 +1,33 @@
-"use client";
-import React from "react";
+import PropertyHeaderImage from '@/components/PropertyHeaderImage'
+import connectDB from '@/config/database'
+import Property from '@/models/Property'
+import Link from 'next/link'
+import { FaArrowLeft } from 'react-icons/fa'
 
-const PropertyPage = () => {
-  return <div>PropertyPage</div>;
-};
+const PropertyPage = async ({ params }) => {
+  await connectDB()
+  const property = await Property.findById(params.id).lean()
 
-export default PropertyPage;
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className='container px-6 py-6 m-auto'>
+          <Link
+            href='/properties'
+            className='flex items-center text-blue-500 hover:text-blue-600'
+          >
+            <FaArrowLeft className='mr-2' /> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section class='bg-blue-50'>
+        {/* Property Info */}
+        <div class='container m-auto py-10 px-6'></div>
+        <div class='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'></div>
+      </section>
+    </>
+  )
+}
+
+export default PropertyPage
